bed63b482e31ffd5c2b9a30ee829388f36693540
killerdroid99 <ajempire09@gmail.com>
2022-09-02 12:50:01 +0530

    Added and fetched pokemon data

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,11 +4,13 @@ import superjson from "superjson";
 
 import { exampleRouter } from "./example";
 import { protectedExampleRouter } from "./protected-example-router";
+import { pokemonRouter } from "./pokemon";
 
 export const appRouter = createRouter()
-  .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", protectedExampleRouter);
+	.transformer(superjson)
+	.merge("example.", exampleRouter)
+	.merge("auth.", protectedExampleRouter)
+	.merge("pokemon.", pokemonRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
